1a46e442d4960c6ca47800749e06f2341edd2f73
kelly r maass <kmaass77627@gmail.com>
2019-12-09 23:37:34 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project 
+  
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+    
+  end 
+  
+  def add_backer(backer)
+   @backers << backer
+ end 
+ 
+ 
+  
+end 
